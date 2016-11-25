1c0fce6bd799b305e8b4df48d598c3595c52aa95
Christopher Holle <chrisaholle@gmail.com>
2016-11-24 23:30:57 -0600

    Moveable object
    This makes a plus sign(pokemon) move around the screen.

--- PokemonGO.cpp
+++ PokemonGO.cpp
@@ -7,27 +7,37 @@ using namespace std;
 int main(int argc, char ** argv)
 {
 
-    SDL_Plotter g(1000,1000);
-    int x,y, xd, yd;
+	SDL_Plotter g(500, 500);
+    int x, y, xd, yd;
     int R,G,B;
-
+    x = g.getCol()/2;
+    y = g.getRow()/2;
+    R = 0;
+    G = 0;
+    B = 0;
     while (!g.getQuit())
     {
-    	x = rand()%g.getCol();
-    	y = rand()%g.getRow();
-    	R = rand()%256;
-    	G = rand()%256;
-    	B = rand()%256;
-    	for(xd = 0; xd < 10 && x + xd < g.getCol(); xd++ ){
-    		for(yd = 0; yd < 10 && y + yd < g.getRow(); yd++ ){
-    	    	g.plotPixel( x + xd, y + yd, R, G, B);
-    	    	g.plotPixel( x + xd, y + yd, 0, G, 0);
-
-    		}
-    	}
+    	g.plotPixel(x,y, 0, 0, 0);
+    	g.plotPixel(x+1,y, 0, 0, 0);
+    	g.plotPixel(x-1,y, 0, 0, 0);
+    	g.plotPixel(x,y+1, 0, 0, 0);
+    	g.plotPixel(x,y-1, 0, 0, 0);
     	g.update();
     	if(g.kbhit()){
-    	    g.getKey();
+    	    if(g.getKey() == UP_ARROW){
+    	    	y--;
+    	    }
+    	    if(g.getKey() == DOWN_ARROW){
+				y++;
+			}
+    	    if(g.getKey() == RIGHT_ARROW){
+				x++;
+			}
+    	    if(g.getKey() == LEFT_ARROW){
+				x--;
+			}
+			g.clear();
     	}
+
     }
 }

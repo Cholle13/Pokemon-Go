b724d32f2d6bbb93ee1e63e9cd11e66d8afc6980
benagueros <benagueros@gmail.com>
2016-11-29 23:05:46 -0600

    Update Pokemon.h

--- Code/Pokemon.h
+++ Code/Pokemon.h
@@ -3,6 +3,7 @@
 
 #include "SDL_Plotter.h"
 #include <fstream>
+#include <vector>
 
 
 using namespace std;
@@ -31,7 +32,9 @@ class Pokemon{
 private:
 
     Point oldLoc;
-    Color picture[256][256];  //Pixel color R,G,B
+    //Color picture[256][256];
+    vector< vector<Color> > picture;
+     //Pixel color R,G,B
     int speed;
     int rows;
     int col;
@@ -39,16 +42,19 @@ private:
     int y;
 
 public:
+
     Pokemon(string);
     Point loc;
     void draw(SDL_Plotter&);
     void erase(SDL_Plotter&);
     void move(DIR);
+    void pokMove(SDL_Plotter& g);
 
 };
 
 
 Pokemon::Pokemon(string filename){
+    picture.resize(256, vector<Color>(256));
     string name = filename;
     ifstream file(name.c_str());
     loc.x = (rand()%900) + 2;
@@ -74,6 +80,7 @@ void Pokemon::draw(SDL_Plotter& g){
             }
         }
     }
+    //picture.clear();
 }
 
 void Pokemon::erase(SDL_Plotter& g){
@@ -103,5 +110,97 @@ void Pokemon::move(DIR d){
 
 }
 
+void Pokemon::pokMove(SDL_Plotter& g){
+    int mov = rand()%4;
+    int freq = rand()%500;
+    if(freq == 250){
+                switch(mov){
+                    case 0: if(loc.y > 1)move(UP);
+                        break;
+                    case 1: if(loc.y < 925)move(DOWN);
+                        break;
+                    case 2: if(loc.x > 25)move(LEFT);
+                        break;
+                    case 3: if(loc.x < 950)move(RIGHT);
+                }
+
+    }
+    erase(g);
+
+}
+
+
+class Menu{
+    private:
+        Point loc;
+        //Color picture[1080][1920];
+        vector< vector<Color> > picture;
+        int rows;
+        int col;
+        int x;
+        int y;
+
+
+    public:
+    Menu(string);
+    void draw(SDL_Plotter&);
+    void erase(SDL_Plotter&);
+
+};
+
+Menu::Menu(string fileName){
+    picture.resize(1000, vector<Color>(1000));
+    string name = fileName;
+    ifstream file(name.c_str());
+    loc.x = 0;
+    loc.y = 0;
+    file >> rows >> col;
+    for(int r = 0; r < rows; r++){
+        for(int c = 0; c < col; c++){
+            file >> picture[r][c].R;
+            file >> picture[r][c].G;
+            file >> picture[r][c].B;
+        }
+    }
+
+
+}
+
+void Menu::draw(SDL_Plotter& menu){
+    for(int r = 0; r < rows; r++){
+        for(int c = 0; c < col; c++){
+            menu.plotPixel(loc.x + c,loc.y + r,picture[r][c].R, picture[r][c].G, picture[r][c].B);
+        }
+    }
+    //picture.clear();
+
+}
+
+void Menu::erase(SDL_Plotter& menu){
+    for(int r =0; r < rows; r++){
+        for(int c = 0; c < col; c++){
+            menu.plotPixel(loc.x + c, loc.y + r, 255, 255, 255);
+        }
+
+    }
+}
+
+string getName(){
+    string pokemon;
+    string name = "marill", name2 = "Omanyte", name3 = "Pikachu", name4 = "Pokemon1", name5= "Squirtle";
+    int num = rand() % 4;
+    switch(num){
+        case 0: pokemon = name;
+            break;
+        case 1: pokemon = name2;
+            break;
+        case 2: pokemon = name3;
+            break;
+        case 3: pokemon = name4;
+            break;
+
+    }
+    return pokemon;
+}
 
 #endif // POKEMON_H_INCLUDED

31b81a8f2a1682ec48f4d2fee7598280e0ade499
benagueros <benagueros@gmail.com>
2016-11-29 23:09:07 -0600

    Add files via upload

---
+++ Code/otherFiles/Menu.h
@@ -0,0 +1,20 @@
+#ifndef MENU_H_INCLUDED
+#define MENU_H_INCLUDED
+#include "SDL_Plotter.h"
+
+enum RGB {R, G, B};
+
+
+
+class Menu{
+    private:
+
+
+
+    public:
+    void draw(SDL_Plotter& onMenu)
+
+}
+
+
+#endif // MENU_H_INCLUDED

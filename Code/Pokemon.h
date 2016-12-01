41b04927f737c8299d35ab3c2f630cdd7b2afdef
Christopher Holle <chrisaholle@gmail.com>
2016-11-30 18:56:23 -0600

    Added sneaky draw function

--- Code/Pokemon.h
+++ Code/Pokemon.h
@@ -19,7 +19,7 @@ struct Point{
 Point::Point(int a, int b){
     x = a;
     y = b;
-
+    
 }
 
 struct Color{
@@ -30,29 +30,53 @@ struct Color{
 };
 class Pokemon{
 private:
-
+    
     Point oldLoc;
     //Color picture[256][256];
     vector< vector<Color> > picture;
-     //Pixel color R,G,B
+    //Pixel color R,G,B
     int speed;
     int rows;
     int col;
     int x;
     int y;
-
+    
 public:
-
+    
     Pokemon(string);
+    Pokemon(int);
     Point loc;
     void draw(SDL_Plotter&);
+    void draw(SDL_Plotter&, string);
     void erase(SDL_Plotter&);
     void move(DIR);
+    Point getOldLoc();
     void pokMove(SDL_Plotter& g);
-
+    
 };
 
-
+Point Pokemon::getOldLoc(){
+    return oldLoc;
+}
+Pokemon::Pokemon(int a){
+    picture.resize(1000, vector<Color>(1000));
+    string bkgrds[5] = {"BabyBack.txt"};
+    string name = bkgrds[a];
+    ifstream file(name.c_str());
+    loc.x = 0;
+    loc.y = 0;
+    oldLoc = loc;
+    file >> rows >> col;
+    for(int r = 0; r < rows; r++){
+        for(int c = 0; c < col; c++){
+            file >> picture[r][c].R;
+            file >> picture[r][c].G;
+            file >> picture[r][c].B;
+        }
+    }
+    file.close();
+    
+}
 Pokemon::Pokemon(string filename){
     picture.resize(256, vector<Color>(256));
     string name = filename;
@@ -69,9 +93,10 @@ Pokemon::Pokemon(string filename){
             file >> picture[r][c].B;
         }
     }
-
+    file.close();
+    
 }
-
+//Basic Draw Function
 void Pokemon::draw(SDL_Plotter& g){
     for(int r = 0; r < rows; r++){
         for(int c = 0; c < col; c++){
@@ -80,18 +105,28 @@ void Pokemon::draw(SDL_Plotter& g){
             }
         }
     }
+    
+}
+
+//Sneaky way to change the Sprite to one of your choosing
+void Pokemon::draw(SDL_Plotter& g, string boy){
+    Pokemon sneak(boy);
+    sneak.loc = loc;
+    sneak.draw(g);
     //picture.clear();
 }
 
-void Pokemon::erase(SDL_Plotter& g){
 
+void Pokemon::erase(SDL_Plotter& g){
+    
     for(int r =0; r < rows; r++){
         for(int c = 0; c < col; c++){
-            g.plotPixel(oldLoc.x + c, oldLoc.y + r, 255, 255, 255);
+            if(picture[r][c].R != 255 || picture[r][c].G != 255 || picture[r][c].B != 255)
+                g.plotPixel(oldLoc.x + c, oldLoc.y + r, 255, 255, 255);
         }
     }
-
-
+    
+    
 }
 
 
@@ -107,45 +142,45 @@ void Pokemon::move(DIR d){
         case RIGHT: loc.x += speed;
             break;
     }
-
+    
 }
 
 void Pokemon::pokMove(SDL_Plotter& g){
     int mov = rand()%4;
     int freq = rand()%500;
     if(freq == 250){
-                switch(mov){
-                    case 0: if(loc.y > 1)move(UP);
-                        break;
-                    case 1: if(loc.y < 925)move(DOWN);
-                        break;
-                    case 2: if(loc.x > 25)move(LEFT);
-                        break;
-                    case 3: if(loc.x < 950)move(RIGHT);
-                }
-
+        switch(mov){
+            case 0: if(loc.y > 1)move(UP);
+                break;
+            case 1: if(loc.y < 925)move(DOWN);
+                break;
+            case 2: if(loc.x > 25)move(LEFT);
+                break;
+            case 3: if(loc.x < 950)move(RIGHT);
+        }
+        
     }
     erase(g);
-
+    
 }
 
-
+// MENU STUFF
 class Menu{
-    private:
-        Point loc;
-        //Color picture[1080][1920];
-        vector< vector<Color> > picture;
-        int rows;
-        int col;
-        int x;
-        int y;
-
-
-    public:
+private:
+    Point loc;
+    //Color picture[1080][1920];
+    vector< vector<Color> > picture;
+    int rows;
+    int col;
+    int x;
+    int y;
+    
+    
+public:
     Menu(string);
     void draw(SDL_Plotter&);
     void erase(SDL_Plotter&);
-
+    
 };
 
 Menu::Menu(string fileName){
@@ -162,18 +197,18 @@ Menu::Menu(string fileName){
             file >> picture[r][c].B;
         }
     }
-
-
+    
+    
 }
 
 void Menu::draw(SDL_Plotter& menu){
     for(int r = 0; r < rows; r++){
         for(int c = 0; c < col; c++){
-            menu.plotPixel(loc.x + c,loc.y + r,picture[r][c].R, picture[r][c].G, picture[r][c].B);
+                menu.plotPixel(loc.x + c,loc.y + r,picture[r][c].R, picture[r][c].G, picture[r][c].B);
         }
     }
     //picture.clear();
-
+    
 }
 
 void Menu::erase(SDL_Plotter& menu){
@@ -181,26 +216,25 @@ void Menu::erase(SDL_Plotter& menu){
         for(int c = 0; c < col; c++){
             menu.plotPixel(loc.x + c, loc.y + r, 255, 255, 255);
         }
-
+        
     }
 }
 
-string getName(){
-    string pokemon;
-    string name = "marill", name2 = "Omanyte", name3 = "Pikachu", name4 = "Pokemon1", name5= "Squirtle";
-    int num = rand() % 4;
-    switch(num){
-        case 0: pokemon = name;
-            break;
-        case 1: pokemon = name2;
-            break;
-        case 2: pokemon = name3;
-            break;
-        case 3: pokemon = name4;
-            break;
+//File names of pokemon stored here
+string getName(int a){
+    string pokemon[15] = {"Marill", "Omanyte", "Pikachu", "pokemon1", "Squirtle", "BabyBack.txt"};
+    //string name = "Marill", name2 = "Omanyte", name3 = "Pikachu", name4 = "pokemon1", name5= "Squirtle";
+    //int num = rand() % 4;
 
-    }
-    return pokemon;
+    return pokemon[a];
+}
+
+//File names of character movements here
+string getCharMove(int a){
+    string character[12] = {"Boy", "DownBoy1", "DownBoy2", "UpBoy", "UpBoy1", "UpBoy2", "RBoy", "RBoy1", "RBoy2",
+                          "LBoy", "LBoy1", "LBoy2"};
+    
+    return character[a];
 }
 
 #endif // POKEMON_H_INCLUDED

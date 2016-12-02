e884865fe0b0426be3da320ffe72096123d3f775
Christopher Holle <chrisaholle@gmail.com>
2016-12-02 17:48:07 -0600

    Create Menu.h

---
+++ Code/Menu.h
@@ -0,0 +1,63 @@
+#ifndef MENU_H_INCLUDED
+#define MENU_H_INCLUDED
+#include "SDL_Plotter.h"
+#include "Pokemon.h"
+
+// MENU STUFF (BEN'S AREA OF EXPERTISE STAY OUT)
+class Menu{
+private:
+    Point loc;
+    //Color picture[1080][1920];
+    vector< vector<Color> > picture;
+    int rows;
+    int col;
+    int x;
+    int y;
+    
+    
+public:
+    Menu(string);
+    void draw(SDL_Plotter&);
+    void erase(SDL_Plotter&);
+    
+};
+
+Menu::Menu(string fileName){
+    picture.resize(1000, vector<Color>(1000));
+    string name = fileName;
+    ifstream file(name.c_str());
+    loc.x = 0;
+    loc.y = 0;
+    file >> rows >> col;
+    for(int r = 0; r < rows; r++){
+        for(int c = 0; c < col; c++){
+            file >> picture[r][c].R;
+            file >> picture[r][c].G;
+            file >> picture[r][c].B;
+        }
+    }
+    
+    
+}
+
+void Menu::draw(SDL_Plotter& menu){
+    for(int r = 0; r < rows; r++){
+        for(int c = 0; c < col; c++){
+            menu.plotPixel(loc.x + c,loc.y + r,picture[r][c].R, picture[r][c].G, picture[r][c].B);
+        }
+    }
+    //picture.clear();
+    
+}
+
+void Menu::erase(SDL_Plotter& menu){
+    for(int r =0; r < rows; r++){
+        for(int c = 0; c < col; c++){
+            menu.plotPixel(loc.x + c, loc.y + r, 255, 255, 255);
+        }
+        
+    }
+}
+
+
+#endif // MENU_H_INCLUDED

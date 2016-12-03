598deef37499c9b7a562658766cc293265d61cae
benagueros <benagueros@gmail.com>
2016-12-03 09:04:29 -0600

    Update Menu.h

--- Code/Menu.h
+++ Code/Menu.h
@@ -3,6 +3,7 @@
 #include "SDL_Plotter.h"
 #include "Pokemon.h"
 
+using namespace std;
 // MENU STUFF (BEN'S AREA OF EXPERTISE STAY OUT)
 class Menu{
 private:
@@ -13,13 +14,15 @@ private:
     int col;
     int x;
     int y;
-    
-    
+
+
 public:
     Menu(string);
     void draw(SDL_Plotter&);
     void erase(SDL_Plotter&);
-    
+    void change(string);
+    void drawNoWhite(SDL_Plotter&);
+
 };
 
 Menu::Menu(string fileName){
@@ -36,8 +39,8 @@ Menu::Menu(string fileName){
             file >> picture[r][c].B;
         }
     }
-    
-    
+
+
 }
 
 void Menu::draw(SDL_Plotter& menu){
@@ -47,7 +50,7 @@ void Menu::draw(SDL_Plotter& menu){
         }
     }
     //picture.clear();
-    
+
 }
 
 void Menu::erase(SDL_Plotter& menu){
@@ -55,9 +58,31 @@ void Menu::erase(SDL_Plotter& menu){
         for(int c = 0; c < col; c++){
             menu.plotPixel(loc.x + c, loc.y + r, 255, 255, 255);
         }
-        
+
     }
 }
 
+void Menu::change(string fileName){
+string name = fileName;
+ifstream file(name.c_str());
+file >> rows >> col;
+    for(int r = 0; r < rows; r++){
+        for(int c = 0; c < col; c++){
+            file >> picture[r][c].R;
+            file >> picture[r][c].G;
+            file >> picture[r][c].B;
+        }
+    }
+}
+
+void Menu::drawNoWhite(SDL_Plotter& g){
+    for(int r = 0; r < rows; r++){
+        for(int c = 0; c < col; c++){
+            if(picture[r][c].R != 255 || picture[r][c].G != 255 || picture[r][c].B != 255){
+                g.plotPixel(loc.x + c,loc.y + r,picture[r][c].R, picture[r][c].G, picture[r][c].B);
+            }
+        }
+    }
+}
 
 #endif // MENU_H_INCLUDED

ca3209bc8d8b1a1c3f55989f374eb3c96fdd843a
Christopher Holle <chrisaholle@gmail.com>
2016-12-04 02:29:39 -0600

    Added Inventory bar/bag

--- Code/main.cpp
+++ Code/main.cpp
@@ -2,7 +2,6 @@
 #include "Pokemon.h"
 #include "SDL_Plotter.h"
 #include "Menu.h"
-#include "Pokeball.h"
 
 using namespace std;
 
@@ -11,49 +10,51 @@ int main(int argc, char ** argv)
     SDL_Plotter g(1000, 1000);
     int x, y;
     //int R,G,B;
-
+    
     Menu background("Menubkrd");
     Menu text("textbox2");
-
+    
     //sets up the Pokemon objects
     Pokemon BackGround(0);
-
+    
     bool select = false;
     bool onMenu = true;
     bool play = false;
-
+    
     //and these(string)
     Pokemon boy(getCharMove(0));
     boy.setLoc(500, 700);
-
+    
     Pokemon pokeball("Pokeball");
-    //Pokemon victory("Victory");
-    //victory.setLoc(111, 111);
+    Pokemon victory(1);
+    victory.setLoc(186, 111);
+    Pokemon inventoryBar(2);
+    Pokemon inventoryBag("InventoryBag");
+    inventoryBar.setLoc(940, 165);
+    inventoryBag.setLoc(935, 750);
     Pokemon pokedex[15];
     init_PokeDex(pokedex);
-
+    
     x = g.getCol()/2;
     y = g.getRow()/2;
+    
     /*
-    //SOUND
-         Mix_OpenAudio( 22050, MIX_DEFAULT_FORMAT, 2, 4096 );
-
-        Mix_Music *music = NULL;
-        music = Mix_LoadMUS( "101-opening.mp3" );
-
-        Mix_Music *music_after_menu = NULL;
-        music_after_menu = Mix_LoadMUS( "background5.mp3" );
-
-        Mix_Chunk *sound = NULL;
-        sound = Mix_LoadWAV( "Laser_Shoot22.wav" );
-    */
-
+     //SOUND
+     Mix_OpenAudio( 22050, MIX_DEFAULT_FORMAT, 2, 4096 );
+     Mix_Music *music = NULL;
+     music = Mix_LoadMUS( "101-opening.mp3" );
+     Mix_Music *music_after_menu = NULL;
+     music_after_menu = Mix_LoadMUS( "background5.mp3" );
+     Mix_Chunk *sound = NULL;
+     sound = Mix_LoadWAV( "Laser_Shoot22.wav" );
+     */
+    
     //declare some fun variables
     int spriteNum = 0, timer = 0, moveCount = 0, poke_Left = 15, inventory_Toggle = 0;
     bool up, down, left, right;
     // play music
-     //Mix_PlayMusic( music, -1 );
-
+    //Mix_PlayMusic( music, -1 );
+    
     //keeps running until ESC key is pressed
     while (!g.getQuit()){
         //loads menu and keeps open until the escape keyh is pressed or s is pressed
@@ -69,49 +70,56 @@ int main(int argc, char ** argv)
             }
             g.update();
         }
-
-
+        
+        
+        //EDIT   HAD TO  DO THIS BECAUSE THE DIALOGUE BOX NO WORKIN
+        //play = true;
+        
         //plays game after done with menu
-            if(!onMenu){
+        if(!onMenu){
             //draw sprites
             BackGround.draw(g);
+            
+            if(inventory_Toggle != 1)
+                inventoryBag.draw(g);
+            else
+                inventoryBar.draw(g);
             //This test if pokemon are alive and if so keep drawing them
-            //alive_draw(poke_Collection, g);
-
+            alive_draw(pokedex, g);
+            
+            if(poke_Left == 0)
+                victory.draw(g);
+            
             //this is to initialize the booleans
             if(timer == 0){
                 up = down = right = left = false;
                 //this is to only draw the pokeball when one of the booleans is true
-            }else if(up || down || right || left)
+            }
+            else if(up || down || right || left){
                 pokeball.draw(g);
-            //Change this to change the pokeballs speed
+            }
+            
+            //Change this to change the pokeball's speed
             pokeball.setSpeed(10);
             boy.draw(g, getCharMove(spriteNum));
             g.update();
-            pokeball.erase(g);
-
-
-
-
-
-            //displays the text box and keeps it up until A is pressed
-            while(!play){
-
-                text.drawNoWhite(g);
-                g.update();
-                g.Sleep(10000);
-                text.change("textbox3");
-                text.drawNoWhite(g);
-                g.update();
-                g.Sleep(5000);
-                play = true;
-
-            }
-
-
+            //pokeball.erase(g);
+            
+            //displays the text box and keeps it up until time expires
+              while(!play){
+             text.drawNoWhite(g);
+             g.update();
+             g.Sleep(10000);
+             text.change("textbox3");
+             text.drawNoWhite(g);
+             g.update();
+             g.Sleep(5000);
+             play = true;
+             }
+            
             //When done with textbox it allows for movement
             if(play){
-
+                
                 //Determines which boy sprite to use based on direction
                 if(g.kbhit()){
                     //Up arrow stuff
@@ -146,12 +154,10 @@ int main(int argc, char ** argv)
                         else
                             spriteNum = 10;
                     }
-
+                    
                     //Finds which way the pokeball needs to move (make function that return loc of ball)
                     //Test if spacebar is pressed. If so, set start location of pokeball
                     if(g.getKey() == ' ' && !down && !up && !right && !left){
-                        //sound
-                        //Mix_PlayChannel( -1, sound, 0);
                         switch(spriteNum){
                                 //With no break if it is 0 it runs until it hits case 2(CLEVER AMIRITE?)
                             case 0:
@@ -179,9 +185,9 @@ int main(int argc, char ** argv)
                                 pokeball.setLoc(boy.loc.x, boy.loc.y + 15);
                                 break;
                         }
-
+                        
                     }
-
+                    
                     //THIS TOGGLES INVENTORY ON/OFF
                     if(g.getKey() == 'I'){
                             inventory_Toggle = 1;
@@ -189,7 +195,7 @@ int main(int argc, char ** argv)
                         inventory_Toggle = 0;
                     }
                 }
-
+                
                 //POKEBALL STUFF
                 //These keep the ball moving - change the moveCount to increase distance shoot
                 //If hits pokemon before moving the distance, stop movement
@@ -200,6 +206,8 @@ int main(int argc, char ** argv)
                         moveCount = 20;
                         poke_Left--;
                     }
+                    if(pokeball.getyLoc() > 989)
+                        moveCount = 20;
                 }
                 if(up && moveCount != 20){
                     pokeball.move(UP);
@@ -208,6 +216,8 @@ int main(int argc, char ** argv)
                         moveCount = 20;
                         poke_Left--;
                     }
+                    if(pokeball.getyLoc() < 11)
+                        moveCount = 20;
                 }
                 if(right && moveCount != 20){
                     pokeball.move(RIGHT);
@@ -216,6 +226,8 @@ int main(int argc, char ** argv)
                         moveCount = 20;
                         poke_Left--;
                     }
+                    if(pokeball.getxLoc() > 989)
+                        moveCount = 20;
                 }
                 if(left && moveCount != 20){
                     pokeball.move(LEFT);
@@ -224,6 +236,8 @@ int main(int argc, char ** argv)
                         moveCount = 20;
                         poke_Left--;
                     }
+                    if(pokeball.getxLoc() < 11)
+                        moveCount = 20;
                 }
                 //This is for when it reaches its destination
                 if(moveCount == 20){
@@ -236,24 +250,27 @@ int main(int argc, char ** argv)
                     spriteNum = boy_StandStill(spriteNum);
                     timer = 1;
                 }
-
+                
                 //Random number for direction
                 //Random number to space out movement of pokemon
                 random_Move(pokedex, g);
-
+                
                 //INVENTORY TOGGLE ON/OFF
                 if(inventory_Toggle == 1){
                     collect_Inventory(pokedex, g);
+                    inventoryBag.erase(g);
                 }
                 if(inventory_Toggle == 0){
                     inventory_Dissapear(pokedex, g);
+                    inventoryBar.erase(g);
+                    
                 }
-
-
+                
+                
             }
         }
     }
-
+    
     return 0;
 }
 //} Magical totem bracket - DO NOT TOUCH

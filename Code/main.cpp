8d85a8a9ac7203919a7b0336b4ecba55e1a47029
Christopher Holle <chrisaholle@gmail.com>
2016-12-04 23:09:12 -0600

    Create main.cpp

---
+++ Code/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Game_Init.h"
+
+using namespace std;
+
+int main(int argc, char ** argv)
+{
+    game_Init();
+    
+    return 0;
+}
+//} Magical totem bracket - DO NOT TOUCH
